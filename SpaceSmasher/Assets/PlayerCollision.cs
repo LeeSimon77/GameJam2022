cf2eef2b45cb1061aabda3b490849a25ee5cb846
LeeSimon77 <ceilidh.simon@gmail.com>
2022-01-29 18:04:49 -0600

    score and edges

--- SpaceSmasher/Assets/PlayerCollision.cs
+++ SpaceSmasher/Assets/PlayerCollision.cs
@@ -1,14 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerCollision : MonoBehaviour
 {
+    private int playerSize = 1;
+    [SerializeField] private Text scoreText;
+
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.CompareTag("Asteroid"))
         {
             Destroy(collision.gameObject);
+            playerSize++;
+            Debug.Log("Player size: " + playerSize);
+            scoreText.text = "Score: " + playerSize;
 
         }
     }
